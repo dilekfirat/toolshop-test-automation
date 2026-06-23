336f3d81b683e02e49f434d9ba94baaca7ae97ff
dilekfirat <dilekfirat.qa@gmail.com>
2026-06-23 14:17:11 +0200

    Add RegistrationPage and user registration test with API integration

--- api/UserApi.ts
+++ api/UserApi.ts
@@ -1,7 +1,8 @@
 import { APIRequestContext, expect } from '@playwright/test';
 
 export class UserApi {
-async createUser(
+
+    async createUser(
     request: APIRequestContext,
     user: any
 ): Promise<void> {
@@ -14,5 +15,21 @@ async createUser(
     );
 
     expect(response.ok()).toBeTruthy();
+}
+
+async loginUser(
+    request: APIRequestContext,
+    user: any
+) {
+
+    return await request.post(
+        'https://api.practicesoftwaretesting.com/users/login',
+        {
+            data: {
+                email: user.email,
+                password: user.password
+            }
+        }
+    );
 }
 }
