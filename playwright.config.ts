27025e1365a7bd7cf0b6bdb19a73ad5ec513f606
dilekfirat <dilekfirat.qa@gmail.com>
2026-07-01 14:50:54 +0200

    Add dotenv and @testiny/automation dependencies, configure Playwright reporter, and create .env.example file

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,6 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
+import 'dotenv/config';
 
 /**
  * Read environment variables from file.
@@ -13,17 +14,33 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
+
   testDir: './tests',
+
   /* Run tests in files in parallel */
   fullyParallel: true,
+
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  //reporter: 'html',
+
+  reporter: [
+    ['list'],
+    ['@testiny/automation/reporters/playwright', {
+    enable: true,
+    project: 'TTT426',
+    sourceName: 'playwright'
+    // API key is loaded from the TESTINY_API_KEY environment variable
+  }]
+],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
